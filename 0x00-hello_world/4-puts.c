1be384f47e99e17b8b6c00ff1eaa8bede5b8e10e
spectersegun <segunomojolowo@gmail.com>
2023-03-19 11:59:41 -0700

    latest

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-/** 
+/**
  * main - this is main function
  *
  * Return: always 0
  */
 
-int main (void){
+int main(void)
+{
 
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
-
