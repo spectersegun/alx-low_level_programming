5cf973c9c5aab4cb277461d8ea0ab3579bd04c36
spectersegun <segunomojolowo@gmail.com>
2023-03-20 01:56:47 -0700

    latest

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ * Return: zero if error and non zero if no error
+ */
+
+int main(void)
+{
+	printf("size of a char: %ld byte(s)\n", sizeof(char));
+	printf("size of an int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
