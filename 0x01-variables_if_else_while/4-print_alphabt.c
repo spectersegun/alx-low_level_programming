2cd9f491f2c73b61134a01dab67671b971665d05
spectersegun <segunomojolowo@gmail.com>
2023-03-20 21:59:54 -0700

    latest

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - origin
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
