875e9e0897365984ad8194982224883ee8196d3c
spectersegun <segunomojolowo@gmail.com>
2023-03-27 19:58:23 -0700

    latest

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_strlen"
 /**
 * print_rev - prints a string in reverse
 * @s: string to print
@@ -18,3 +17,14 @@ void print_rev(char *s)
  * @s: string
  *Return: returns lenght;
  */
+int _strlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
