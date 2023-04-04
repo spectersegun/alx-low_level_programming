a6b00c7cb96c8566aec2b0375e1482bb5823b112
spectersegun <segunomojolowo@gmail.com>
2023-04-03 23:18:52 -0700

    latets

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _puts_recursion - Funcion that prints a string
+* @s: string to be printed
+* Return: printed string
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
